badfc038b10004b38fff80246c354c763b3d9c2e
Samuel BIJOU <samuel.bijou@gmail.com>
2017-06-28 02:40:31 +0200

    Add Upload System

---
+++ src/main/java/com/esgi/extranet/system/UploadSystem.java
@@ -0,0 +1,89 @@
+package com.esgi.extranet.system ;
+
+import javax.imageio.ImageIO ;
+import java.awt.image.BufferedImage ;
+import java.io.* ;
+import java.net.ServerSocket ;
+import java.net.Socket ;
+
+/**
+ * Created by Samuel Bijou on 28/06/2017.
+ */
+public class UploadSystem
+{
+
+    public void uploadToServer(String imagePath) throws IOException
+    {
+        Socket soc ;
+        BufferedImage img = null ;
+        soc = new Socket("localhost", 4000) ;
+
+        try
+        {
+            img = ImageIO.read(new File(imagePath)) ;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream() ;
+
+            ImageIO.write(img, getImageFormat(imagePath), baos) ;
+            baos.flush() ;
+
+            byte[] bytes = baos.toByteArray() ;
+            baos.close() ;
+
+            OutputStream out = soc.getOutputStream() ;
+            DataOutputStream dos = new DataOutputStream(out) ;
+
+            dos.writeInt(bytes.length) ;
+            dos.write(bytes, 0, bytes.length) ;
+
+            dos.close() ;
+            out.close() ;
+
+        }
+
+        catch (Exception e)
+        {
+            soc.close() ;
+        }
+
+        soc.close() ;
+    }
+
+    public void receiveUpload() throws IOException
+    {
+        ServerSocket server = null ;
+        Socket socket ;
+        server = new ServerSocket(4000) ;
+
+        socket = server.accept() ;
+
+        InputStream in = socket.getInputStream() ;
+        DataInputStream dis = new DataInputStream(in) ;
+
+        int len = dis.readInt() ;
+
+        byte[] data = new byte[len] ;
+        dis.readFully(data) ;
+        dis.close() ;
+        in.close() ;
+
+        InputStream ian = new ByteArrayInputStream(data) ;
+        BufferedImage bImage = ImageIO.read(ian) ;
+    }
+
+    public String getImageFormat(String imagePath)
+    {
+        String imageExtension = imagePath.substring(imagePath.lastIndexOf(".") + 1, imagePath.length()) ;
+
+        return imageExtension ;
+    }
+
+    public String saveImage()
+    {
+        String imageServerPath = "" ;
+
+        // Do Something
+
+        return imageServerPath ;
+    }
+
+}
