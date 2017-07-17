95021e8e5c4b01d393e6186e88f76754c965e0a4
Yukenigon <timothee.arnauld1@gmail.com>
2017-07-18 00:13:08 +0200

    Front -> js and css folders, absolute path => test bugfix

--- src/main/java/com/esgi/extranet/ExtranetApplication.java
+++ src/main/java/com/esgi/extranet/ExtranetApplication.java
@@ -3,13 +3,16 @@ package com.esgi.extranet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @org.springframework.context.annotation.Configuration
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableTransactionManagement
-public class ExtranetApplication {
+@EnableWebMvc
+public class ExtranetApplication extends WebMvcAutoConfiguration {
 	public static void main(String[] args) {
 		SpringApplication.run(ExtranetApplication.class, args);
 	}
