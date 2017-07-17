6f39e6fd28bf7706d228e1c19008e9cef4110a01
Yukenigon <timothee.arnauld1@gmail.com>
2017-07-18 00:17:40 +0200

    Front -> js and css folders, absolute path => test bugfix

--- src/main/java/com/esgi/extranet/ExtranetApplication.java
+++ src/main/java/com/esgi/extranet/ExtranetApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -11,9 +15,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableTransactionManagement
-@EnableWebMvc
-public class ExtranetApplication extends WebMvcAutoConfiguration {
+@EnableWebMvcSecurity
+public class ExtranetApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(ExtranetApplication.class, args);
 	}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**"); // #3
+    }
 }
