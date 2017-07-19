ecdd11ff6cb2440266f4d6f8f1ce745605ed8ced
Yukenigon <timothee.arnauld1@gmail.com>
2017-07-19 14:02:09 +0200

    Error 404

--- src/main/java/com/esgi/extranet/MappingController.java
+++ src/main/java/com/esgi/extranet/MappingController.java
@@ -1,11 +1,11 @@
 package com.esgi.extranet;
 
 import com.esgi.extranet.login.MailClient;
+import edu.umd.cs.findbugs.classfile.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
